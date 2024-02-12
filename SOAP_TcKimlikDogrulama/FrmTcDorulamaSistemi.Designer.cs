81eb9a65e573db2c3b23176800ed485fe13b107e
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2024-02-12 09:48:15 +0300

    icon ve anasayfa resmi düzenlendi

--- SOAP_TcKimlikDogrulama/FrmTcDorulamaSistemi.Designer.cs
+++ SOAP_TcKimlikDogrulama/FrmTcDorulamaSistemi.Designer.cs
@@ -44,78 +44,87 @@ namespace SOAP_TcKimlikDogrulama
             // lblDogumYili
             // 
             this.lblDogumYili.AutoSize = true;
-            this.lblDogumYili.Location = new System.Drawing.Point(160, 171);
+            this.lblDogumYili.Location = new System.Drawing.Point(107, 111);
+            this.lblDogumYili.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblDogumYili.Name = "lblDogumYili";
-            this.lblDogumYili.Size = new System.Drawing.Size(94, 20);
+            this.lblDogumYili.Size = new System.Drawing.Size(63, 13);
             this.lblDogumYili.TabIndex = 23;
             this.lblDogumYili.Text = "Doğum Yılı=";
             // 
             // lblSoyad
             // 
             this.lblSoyad.AutoSize = true;
-            this.lblSoyad.Location = new System.Drawing.Point(183, 132);
+            this.lblSoyad.Location = new System.Drawing.Point(122, 86);
+            this.lblSoyad.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblSoyad.Name = "lblSoyad";
-            this.lblSoyad.Size = new System.Drawing.Size(71, 20);
+            this.lblSoyad.Size = new System.Drawing.Size(49, 13);
             this.lblSoyad.TabIndex = 22;
             this.lblSoyad.Text = "Soyad = ";
             // 
             // lblAd
             // 
             this.lblAd.AutoSize = true;
-            this.lblAd.Location = new System.Drawing.Point(208, 85);
+            this.lblAd.Location = new System.Drawing.Point(139, 55);
+            this.lblAd.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblAd.Name = "lblAd";
-            this.lblAd.Size = new System.Drawing.Size(46, 20);
+            this.lblAd.Size = new System.Drawing.Size(32, 13);
             this.lblAd.TabIndex = 21;
             this.lblAd.Text = "Ad = ";
             // 
             // lblTc
             // 
             this.lblTc.AutoSize = true;
-            this.lblTc.Location = new System.Drawing.Point(97, 42);
+            this.lblTc.Location = new System.Drawing.Point(65, 27);
+            this.lblTc.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblTc.Name = "lblTc";
-            this.lblTc.Size = new System.Drawing.Size(157, 20);
+            this.lblTc.Size = new System.Drawing.Size(107, 13);
             this.lblTc.TabIndex = 20;
             this.lblTc.Text = "TC Kimlik Numarası =";
             // 
             // txtDogumYili
             // 
-            this.txtDogumYili.Location = new System.Drawing.Point(283, 171);
+            this.txtDogumYili.Location = new System.Drawing.Point(189, 111);
+            this.txtDogumYili.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtDogumYili.MaxLength = 4;
             this.txtDogumYili.Name = "txtDogumYili";
-            this.txtDogumYili.Size = new System.Drawing.Size(284, 26);
+            this.txtDogumYili.Size = new System.Drawing.Size(191, 20);
             this.txtDogumYili.TabIndex = 19;
             this.txtDogumYili.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDogumYili_KeyPress);
             // 
             // txtSoyad
             // 
-            this.txtSoyad.Location = new System.Drawing.Point(283, 132);
+            this.txtSoyad.Location = new System.Drawing.Point(189, 86);
+            this.txtSoyad.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtSoyad.Name = "txtSoyad";
-            this.txtSoyad.Size = new System.Drawing.Size(284, 26);
+            this.txtSoyad.Size = new System.Drawing.Size(191, 20);
             this.txtSoyad.TabIndex = 18;
             this.txtSoyad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSoyad_KeyPress);
             // 
             // txtAd
             // 
-            this.txtAd.Location = new System.Drawing.Point(283, 85);
+            this.txtAd.Location = new System.Drawing.Point(189, 55);
+            this.txtAd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtAd.Name = "txtAd";
-            this.txtAd.Size = new System.Drawing.Size(284, 26);
+            this.txtAd.Size = new System.Drawing.Size(191, 20);
             this.txtAd.TabIndex = 17;
             this.txtAd.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAd_KeyPress);
             // 
             // txtTc
             // 
-            this.txtTc.Location = new System.Drawing.Point(283, 42);
+            this.txtTc.Location = new System.Drawing.Point(189, 27);
+            this.txtTc.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtTc.MaxLength = 11;
             this.txtTc.Name = "txtTc";
-            this.txtTc.Size = new System.Drawing.Size(284, 26);
+            this.txtTc.Size = new System.Drawing.Size(191, 20);
             this.txtTc.TabIndex = 16;
             this.txtTc.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTc_KeyPress);
             // 
             // btnTcDogrula
             // 
-            this.btnTcDogrula.Location = new System.Drawing.Point(283, 232);
+            this.btnTcDogrula.Location = new System.Drawing.Point(189, 151);
+            this.btnTcDogrula.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnTcDogrula.Name = "btnTcDogrula";
-            this.btnTcDogrula.Size = new System.Drawing.Size(284, 57);
+            this.btnTcDogrula.Size = new System.Drawing.Size(189, 37);
             this.btnTcDogrula.TabIndex = 15;
             this.btnTcDogrula.Text = "TC Kimlik Numarası Doğrula";
             this.btnTcDogrula.UseVisualStyleBackColor = true;
@@ -123,9 +132,9 @@ namespace SOAP_TcKimlikDogrulama
             // 
             // FrmTcDorulamaSistemi
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(597, 309);
+            this.ClientSize = new System.Drawing.Size(398, 201);
             this.Controls.Add(this.lblDogumYili);
             this.Controls.Add(this.lblSoyad);
             this.Controls.Add(this.lblAd);
@@ -136,6 +145,7 @@ namespace SOAP_TcKimlikDogrulama
             this.Controls.Add(this.txtTc);
             this.Controls.Add(this.btnTcDogrula);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FrmTcDorulamaSistemi";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Load += new System.EventHandler(this.FrmTcDorulamaSistemi_Load);
